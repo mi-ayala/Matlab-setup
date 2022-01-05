ad54daea70726ae25fafc211faa49b82ce6c5ba4
Miguel Ayala <71942809+mi-ayala@users.noreply.github.com>
2022-01-05 13:15:16 -0500

    Update startup.m

--- startup.m
+++ startup.m
@@ -1,8 +1,19 @@
 
-%%%
 
+
+
+
+
+%%% Add Research forlder to search path
+addpath(genpath('~/Research'))
+
+%%% Plotting stuff
 set(0, 'DefaultLineLineWidth' , 2, ...
        'defaultaxesfontsize'  ,12, ...
        'defaultaxeslinewidth' , 7, ...
        'defaultpatchlinewidth', 7);
+       
+%%% Start Intlab
+
+       
   
